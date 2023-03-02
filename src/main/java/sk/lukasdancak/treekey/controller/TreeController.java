041f4bdd0d46bdecf882606b59c2c34cc267df8f
lukasdancak <lukas.dancak@gmail.com>
2023-03-02 13:56:04 +0100

    aplication.properties + H2 and Data JPA dependencies

--- src/main/java/sk/lukasdancak/treekey/controller/TreeController.java
+++ src/main/java/sk/lukasdancak/treekey/controller/TreeController.java
@@ -1,9 +1,7 @@
 package sk.lukasdancak.treekey.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,10 +10,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/trees")
 public class TreeController {
-    private List<String> trees = new ArrayList<String>(Arrays.asList("sdfsdfdsf","sdfdsfdsf"));
+    private List<String> trees = new ArrayList<String>();
 
     @GetMapping()
     List<String> get() {
         return trees;
+    }
+    @PostMapping()
+    void post(@RequestBody String tree){
+        trees.add(tree);
     }
 }
