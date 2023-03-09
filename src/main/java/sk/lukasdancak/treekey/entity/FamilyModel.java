1fe1f52566e63bc0a80ff3c7940a8653a4864ee9
lukasdancak <lukas.dancak@gmail.com>
2023-03-09 22:53:47 +0100

    class FamilyModel finished

--- src/main/java/sk/lukasdancak/treekey/entity/FamilyModel.java
+++ src/main/java/sk/lukasdancak/treekey/entity/FamilyModel.java
@@ -2,34 +2,55 @@ package sk.lukasdancak.treekey.entity;
 
 import jakarta.persistence.*;
 
+import java.io.Serializable;
 import java.util.Collection;
 
 @Entity
 @Table(name = "families")
-public class FamilyModel {
+public class FamilyModel implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String name;
-
-
-    public FamilyModel() {
-        this(null,"");
-    }
-
-    public FamilyModel(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
+    private String name; //name of tree family - meno celade
+    private String description; // description of family - popis celade
     @OneToMany(mappedBy = "family")
     private Collection<TreeModel> trees;
 
-//    public Collection<TreeModel> getTrees() {
-//        return trees;
-//    }
-//
-//    public void setTrees(Collection<TreeModel> trees) {
-//        this.trees = trees;
-//    }
+    public FamilyModel() {
+    }
+
+    public FamilyModel(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTrees(Collection<TreeModel> trees) {
+        this.trees = trees;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Collection<TreeModel> getTrees() {
+        return trees;
+    }
+
+
 }
