645a8a2abe17845e90283bb59301bf470f31cdbe
lukasdancak <lukas.dancak@gmail.com>
2023-03-28 01:05:31 +0200

    basic controller BasicController + dependency for thymeleaf-dialect + basic homepage.html + basic layout

---
+++ src/main/java/sk/lukasdancak/treekey/controller/BasicController.java
@@ -0,0 +1,13 @@
+package sk.lukasdancak.treekey.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BasicController {
+
+    @RequestMapping("/")
+    public String mainPage() {
+        return "home";
+    }
+}
