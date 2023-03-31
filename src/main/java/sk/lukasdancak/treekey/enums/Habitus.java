b1a10d19020f4e1850b5fef6ee180ee2b5723655
lukasdancak <lukas.dancak@gmail.com>
2023-04-01 00:47:56 +0200

    enum Habitus: members + consructor

--- src/main/java/sk/lukasdancak/treekey/enums/Habitus.java
+++ src/main/java/sk/lukasdancak/treekey/enums/Habitus.java
@@ -1,5 +1,14 @@
 package sk.lukasdancak.treekey.enums;
 
 public enum Habitus {
-    ker, strom, liana;
+    KER("ker", "fdgdfghfgh fgh fgh fgh fgh  f hfgh fgh dfgh fgh"),
+    STROM("strom", "fdgd gfd gdfg dfg dfg dfg df"),
+    LIANA("liana", "dfg gfd gdf dfg dfg dfgdfg dgd fgdfg dfg dfg");
+    String name;
+    String description;
+
+    Habitus(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 }
