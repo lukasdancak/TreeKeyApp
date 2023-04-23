e76fabcd14964f03b93d1b94744db135524ae78f
lukasdancak <lukas.dancak@gmail.com>
2023-04-24 00:33:51 +0200

    work on search form

--- src/main/java/sk/lukasdancak/treekey/controller/BasicController.java
+++ src/main/java/sk/lukasdancak/treekey/controller/BasicController.java
@@ -1,7 +1,12 @@
 package sk.lukasdancak.treekey.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import sk.lukasdancak.treekey.dto.TreeDTO;
+import sk.lukasdancak.treekey.dto.TreeSearchDTO;
 
 @Controller
 public class BasicController {
@@ -11,8 +16,13 @@ public class BasicController {
         return "home";
     }
 
-    @RequestMapping("/searchnojs")
-    public String searchNoJSPage() {
+    @RequestMapping("/searchtree")
+    public String searchNoJSPage(Model model, @RequestParam String habitusName) {
+        TreeSearchDTO treeSearchRequest = new TreeSearchDTO(habitusName);
+        // check data of treeSearchRequest in database
+        //if OK then get the list of finded trees
+        // add list to model
+        // return page
         return "searchnojs";
     }
 }
--- src/main/java/sk/lukasdancak/treekey/dto/TreeSearchDTO.java
+++ src/main/java/sk/lukasdancak/treekey/dto/TreeSearchDTO.java
@@ -1,10 +1,8 @@
 package sk.lukasdancak.treekey.dto;
 
 
-public record TreeDTO(String latinName, String slovakName, String familyName/*, FamilyModel family/*, String habitus, String rhytidome,
+public record TreeSearchDTO(String habitus/*, String rhytidome,
                       String leaf, String flower, String fruit, String description, String imageFolder*/) {
-
-
 
 
 }
